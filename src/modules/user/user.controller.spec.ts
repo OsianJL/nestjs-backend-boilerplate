0a9687bd08dcf99a983732cbc211c037402cdd10
Osián <149536063+OsianJL@users.noreply.github.com>
2025-06-30 19:16:33 +0200

    linter problems solved

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,20 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/user.dto';
-import { User } from '@prisma/client';
 
 describe('UserController', () => {
   let controller: UserController;
-  let userService: jest.Mocked<UserService>;
 
-  const mockUserService: jest.Mocked<UserService> = {
+  const mockUserService = {
     createUser: jest.fn(),
     getAllUsers: jest.fn(),
+    findByEmail: jest.fn(),
     getUserById: jest.fn(),
     updateUser: jest.fn(),
     deleteUser: jest.fn(),
-  } as any;
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,7 +26,6 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
-    userService = module.get(UserService);
   });
 
   it('should be defined', () => {
